639dcaf04e8919dc4c0775ead49adc725716dd2b
TZC <3326192368@qq.com>
2025-02-08 21:55:20 +0800

    我的网站直接404了现在，我这回把.php文件撤销到最初状态了。

--- forms/contact.php
+++ forms/contact.php
@@ -7,9 +7,9 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = '3326192368@qq.com';
+  $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = 'forms/contact.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -19,19 +19,19 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];//用户填写的
+  $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
+  /*
   $contact->smtp = array(
-    'host' => 'smtp.qq.com',
-    'username' => '3326192368@qq.com',
-    'password' => 'ztdgsnuhllmfcjea',
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
     'port' => '587'
   );
-  
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
